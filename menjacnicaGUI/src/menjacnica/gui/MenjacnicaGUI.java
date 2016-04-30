ce03c4cc7889f63f7ae562d66b7bb4ea8eb06294
andrijasvrkota <andrija.svrkota@gmail.com>
2016-04-30 15:33:04 +0200

    Modifikovan glavni prozor, dodate ikonice i precice za stavke padajuceg menija

--- menjacnicaGUI/src/menjacnica/gui/MenjacnicaGUI.java
+++ menjacnicaGUI/src/menjacnica/gui/MenjacnicaGUI.java
@@ -16,6 +16,11 @@ import javax.swing.JTable;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import java.awt.Toolkit;
+import javax.swing.KeyStroke;
+import java.awt.event.KeyEvent;
+import java.awt.event.InputEvent;
+import javax.swing.ImageIcon;
 
 public class MenjacnicaGUI extends JFrame {
 
@@ -36,6 +41,7 @@ public class MenjacnicaGUI extends JFrame {
 	private JMenuItem mntmOpen;
 	private JMenuItem mntmSave;
 	private JMenuItem mntmExit;
+	private JMenuItem mntmAbout;
 
 	/**
 	 * Launch the application.
@@ -57,6 +63,7 @@ public class MenjacnicaGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public MenjacnicaGUI() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(MenjacnicaGUI.class.getResource("/resources/paree.png")));
 		setTitle("Menjacnica");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 623, 386);
@@ -73,7 +80,7 @@ public class MenjacnicaGUI extends JFrame {
 	private JPanel getEastPanel() {
 		if (eastPanel == null) {
 			eastPanel = new JPanel();
-			eastPanel.setPreferredSize(new Dimension(100, 10));
+			eastPanel.setPreferredSize(new Dimension(110, 10));
 			eastPanel.add(getBtnDodajKurs());
 			eastPanel.add(getBtnIzbrisiKurs());
 			eastPanel.add(getBtnIzvrsiZamenu());
@@ -100,18 +107,21 @@ public class MenjacnicaGUI extends JFrame {
 	private JButton getBtnDodajKurs() {
 		if (btnDodajKurs == null) {
 			btnDodajKurs = new JButton("Dodaj Kurs");
+			btnDodajKurs.setPreferredSize(new Dimension(100, 23));
 		}
 		return btnDodajKurs;
 	}
 	private JButton getBtnIzbrisiKurs() {
 		if (btnIzbrisiKurs == null) {
 			btnIzbrisiKurs = new JButton("Izbrisi Kurs");
+			btnIzbrisiKurs.setPreferredSize(new Dimension(100, 23));
 		}
 		return btnIzbrisiKurs;
 	}
 	private JButton getBtnIzvrsiZamenu() {
 		if (btnIzvrsiZamenu == null) {
 			btnIzvrsiZamenu = new JButton("Izvrsi Zamenu");
+			btnIzvrsiZamenu.setPreferredSize(new Dimension(100, 23));
 		}
 		return btnIzvrsiZamenu;
 	}
@@ -163,25 +173,37 @@ public class MenjacnicaGUI extends JFrame {
 	private JMenu getMnNewMenu_1() {
 		if (mnNewMenu_1 == null) {
 			mnNewMenu_1 = new JMenu("Help");
+			mnNewMenu_1.add(getMntmAbout());
 		}
 		return mnNewMenu_1;
 	}
 	private JMenuItem getMntmOpen() {
 		if (mntmOpen == null) {
 			mntmOpen = new JMenuItem("Open");
+			mntmOpen.setIcon(new ImageIcon(MenjacnicaGUI.class.getResource("/com/sun/java/swing/plaf/windows/icons/TreeOpen.gif")));
+			mntmOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
 		}
 		return mntmOpen;
 	}
 	private JMenuItem getMntmSave() {
 		if (mntmSave == null) {
 			mntmSave = new JMenuItem("Save");
+			mntmSave.setIcon(new ImageIcon(MenjacnicaGUI.class.getResource("/com/sun/java/swing/plaf/windows/icons/FloppyDrive.gif")));
+			mntmSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
 		}
 		return mntmSave;
 	}
 	private JMenuItem getMntmExit() {
 		if (mntmExit == null) {
 			mntmExit = new JMenuItem("Exit");
+			mntmExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.ALT_MASK));
 		}
 		return mntmExit;
+	}
+	private JMenuItem getMntmAbout() {
+		if (mntmAbout == null) {
+			mntmAbout = new JMenuItem("About");
+		}
+		return mntmAbout;
 	}
 }
--- menjacnicaGUI/src/menjacnicaGUI/MenjacnicaGUI.java
+++
@@ -1,5 +0,0 @@
-package menjacnicaGUI;
-
-public class MenjacnicaGUI {
-
-}
